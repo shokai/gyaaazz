9122ad3f8499211862de9ccbc6e027e8d516259f
Sho Hashimoto <hashimoto@shokai.org>
2011-02-17 07:10:36 +0900

    first commit

---
+++ models/page.rb
@@ -0,0 +1,16 @@
+require 'digest/md5'
+
+class Page
+  include Mongoid::Document
+  field :name
+  field :time, :type => Integer
+  field :body
+
+  def to_hash
+    {
+      :name => name,
+      :time => time,
+      :body => body
+    }
+  end
+end
