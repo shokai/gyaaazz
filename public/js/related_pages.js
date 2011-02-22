9e50d24cc465fb5cc01b2c96c236f6990ee25283
Sho Hashimoto <hashimoto@shokai.org>
2011-02-22 15:16:02 +0900

    show related pages

--- public/js/related_pages.js
+++ public/js/related_pages.js
@@ -1,14 +1,15 @@
 $(function(){
     $.getJSON(app_root+'/api/search.json?word='+page_name, function(res){
-        console.log(res);
         if(res.error){
-            
         }
         else{
             res.forEach(function(i){
-                var box = $('<div>');
+                var box = $('<span>');
                 var a = $('<a>').append(i.name).attr('href',app_root+'/'+i.name);
                 box.append(a);
+                box.click(function(){
+                    location.href = app_root+'/'+i.name;
+                });
                 $('#related_pages').append(box);
             });
         }
